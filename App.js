96c70e7042bc406cf7e4c9e2ddb3b3835349ffdc
Lumikin <lumikomio02@gmail.com>
2026-02-12 15:19:35 -0300

    Feat: Adicioando email e senha

--- App.js
+++ App.js
@@ -1,21 +1,42 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
-import "./assets/icon.png";
-
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  StyleSheet,
+  Alert,
+} from "react-native";
 export default function App() {
+  // Criar estados - memorias
+  const [email, setEmail] = useState("");
+  const [senha, setSenha] = useState("");
+
+  const realizarLogin = () => {
+    if (email === "" || senha === "") {
+      Alert.alert("Atenção, preencha todos os campos");
+    } else {
+      Alert.alert("Login realizado", "Bem vindo", +email);
+    }
+  };
   return (
-    <View style={styles.container}>
-      <Text>brat</Text>
-      <StatusBar style="auto" />
-    </View>
+    <view style={styles.conteiner}>
+      <Text style={styles.titulo}> Área do login </Text>
+      <TextInput
+        placeholder="Digite seu e-mail"
+        style={styles.input}
+        onChangeText={setEmail}
+        keyboardType="email-address"
+      />
+      <TextInput
+        placeholder="Digite a sua senha"
+        style={styles.input}
+        secureTextEntry={true}
+        onChangeText={setSenha}
+      />
+      
+    </view>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#8ace00",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+const styles = StyleSheet.create({});
