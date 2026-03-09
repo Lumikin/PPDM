6fc3c109ccbf2a0a27ce5e7eff82b9d7062db763
Lumikin <lumikomio02@gmail.com>
2026-03-09 13:25:43 -0300

    Feat: Adiciando separador e blocos na tela

--- App.js
+++ App.js
@@ -1,11 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      {/* View 1 */}
+      <Text>View dentro da View principal</Text>
+      <View style={{ width: 300, height: 50, backgroundColor: "pink" }}></View>
+      <View style={styles.separador}> </View>
+      {/* View 2 */}
+      <Text>View dentro da View principal</Text>
+      <View style={{ width: 300, height: 50, backgroundColor: "pink" }}></View>
+      {/* View 3 */}
+      <Text>View dentro da View principal</Text>
+      <View style={{ width: 300, height: 50, backgroundColor: "pink" }}></View>
     </View>
   );
 }
@@ -13,8 +22,16 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  separador: {
+    marginBottom: 5,
+    marginTop: 5,
+    height: 1,
+    width: "95%",
+    backgroundColor: "gray",
+    elevation: 2,
   },
 });
