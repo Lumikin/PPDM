55c5463d5c2b9487ab0c745ab312ef7b8f29336f
Lumikin <lumikomio02@gmail.com>
2026-03-16 14:20:49 -0300

    Refatorar App.js para usar SafeAreaView e melhorar a estrutura do componente

--- App.js
+++ App.js
@@ -1,21 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { AntDesign, FontAwesome } from "@expo/vector-icons";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
       <StatusBar style="auto" />
-    </View>
+      <Text> Trabalhando com icones </Text>
+
+      <FontAwesome name="home" size={32} color="purple"></FontAwesome>
+      <AntDesign.Button name="home"></AntDesign.Button>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
