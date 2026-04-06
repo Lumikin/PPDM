280ad1011244c9ba20332165fa1d1022a416693c
Lumikin <lumikomio02@gmail.com>
2026-04-06 15:51:25 -0300

    Inicialização do repositorio

---
+++ src/screens/Contato/index.js
@@ -0,0 +1,34 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, TouchableOpacity } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+export default function Contato() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <StatusBar style="auto" />
+      <Text style={{ fontWeight: "bold", fontSize: 25 }}>
+        Nossos contatos
+      </Text>
+      <Text style={styles.Text}> Telefone: +55 (11) 4002-8922</Text>
+      <Text style={styles.Text}> WhatsApp: +55 (11) 99999-0000</Text>
+      <Text style={styles.Text}>
+        Endereço: Praça Dom Máximo, 420, Térreo/Loja
+      </Text>
+      <Text style={styles.Text}> CNPJ: 00.000.000/0001-00</Text>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#eef3e4",
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 8,
+    gap: 5,
+  },
+  Text: {
+    fontSize: 16,
+  },
+});
---
+++ src/screens/Home/index.js
@@ -0,0 +1,68 @@
+import { StatusBar } from "expo-status-bar";
+import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { useNavigation } from "@react-navigation/native";
+
+export default function Home() {
+  const navigation = useNavigation();
+  return (
+    <SafeAreaView style={styles.container}>
+      <StatusBar style="auto" />
+
+      <Image
+        source={require("../../../assets/image.png")}
+        style={styles.image}
+      />
+
+      <Text style={{ fontWeight: "bold", fontSize: 40 }}>Miranda's Market</Text>
+
+      <Text style={{ fontSize: 17, margin: 7 }}>
+        Somos um mercado focado em produtos para alimentação saudavel,
+        priorizamos alimentos frescos como frutas, hortaliças e grãos de alta
+        qualidade.
+      </Text>
+
+      <TouchableOpacity
+        style={styles.botao}
+        onPress={() => navigation.navigate("Produtos")}
+      >
+        <Text style={styles.Text}>Produtos</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        style={styles.botao}
+        onPress={() => navigation.navigate("Contato")}
+      >
+        <Text style={styles.Text}>Contato</Text>
+      </TouchableOpacity>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#eef3e4",
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 8,
+    gap: 5,
+  },
+  botao: {
+    backgroundColor: "#4CAF50",
+    width: "90%",
+    height: 50,
+    borderRadius: 25,
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 15,
+  },
+  image: {
+    width: "40%",
+    height: "20%",
+    borderRadius: 20,
+  },
+  Text: {
+    fontWeight: "bold",
+  },
+});
